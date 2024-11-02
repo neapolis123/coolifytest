e8b0267ea7f92219253f49b93720aaac0b6c8b38
neapolis123 <spirovsk@gmail.com>
2024-11-02 13:26:13 +0100

    Update m.py

--- m.py
+++ m.py
@@ -1,10 +1,14 @@
 import psycopg2
-
+import time
 
 conn = psycopg2.connect("postgres://postgres:3QUHdz5M0KbCVK68m5lZmMPN0ontmaXIHZL0rTuvjXe2gvryLldXE6z3i1UHySac@rg8wc8so4sc8o44wowwos08g:5432/cars" )
 
 cur = conn.cursor()
 
-result = cur.execute("INSERT INTO cars VALUES ('Audi', 'A5', 2024)" )
+result = cur.execute("INSERT INTO cars VALUES ('Aston Martin', 'Vanquish', 2019)" )
 conn.commit()
-print(result)
+print('successful insert')
+
+while True:
+    time.sleep(5)
+    print('slept for 5s')
